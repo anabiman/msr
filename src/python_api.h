2014389cdf0a8d8534ce17d616bcfeab632d238d
Andrew-AbiMansour <andrew.gaam@gmail.com>
2019-11-13 01:41:01 -0500

    added python/api files

---
+++ src/python_api.h
@@ -0,0 +1,9 @@
+#ifndef PYTHON_API
+#define PYTHON_API
+
+#include <iostream>
+
+//PetscErrorCode NewtonIter(std::vector<Vec>&, std::vector<Vec>&, PetscInt*, PetscInt*, Vec&, Mat&, Mat&, char*, PetscReal);
+extern double foo(double a){  return a; }
+
+#endif
